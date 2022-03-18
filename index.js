c416c568a79576bf6c75fd35c6c6537999fc476d
hector <hpulido1021@gmail.com>
2022-03-18 15:25:41 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+
+function writeCards(arr) {
+    let name =[];
+    for (let i = 0; i < arr.length; i++) {
+    name.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+  return name;
+}
+
+
+
+function countDown(){
+   let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown --);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
